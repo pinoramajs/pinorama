07b345af5bf0c480965769bca9b3ab3a80420090
Francesco Pasqua <cesconix@me.com>
2023-12-16 15:39:01 +0100

    feat(pino-pinorama): add basic implementation

--- packages/pino-pinorama/src/index.ts
+++ packages/pino-pinorama/src/index.ts
@@ -1,10 +1,35 @@
 import abstractTransport from "pino-abstract-transport"
-import PinoramaClient from "pinorama-client"
+import { PinoramaClient } from "pinorama-client"
+import type { PinoramaClientOptions } from "pinorama-client"
+import type { Transform } from "node:stream"
 
-type PinoramaTransportOptions = {}
+type PinoramaTransportOptions = {
+  url: string
+  maxRetries: number
+  retryInterval: number
+}
 
 export default async function pinoramaTransport(
-  options: PinoramaTransportOptions
+  opts: PinoramaTransportOptions
 ) {
-  const client = new PinoramaClient()
+  const clientOpts: PinoramaClientOptions = {
+    baseUrl: opts.url,
+    maxRetries: opts.maxRetries || 5,
+    retryInterval: opts.retryInterval || 1000
+  }
+
+  const client = new PinoramaClient(clientOpts)
+
+  const buildFn = async (source: Transform) => {
+    client.bulkInsert(source, {
+      batchSize: 1000,
+      flushInterval: 5000
+    })
+  }
+
+  const closeFn = async () => {
+    // TODO: flush client buffer
+  }
+
+  return abstractTransport(buildFn, { close: closeFn })
 }
