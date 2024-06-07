b56e6b50d7fd72b1a0c1f0f8e3369f61e5150aee
Francesco Pasqua <cesconix@me.com>
2024-06-08 00:22:01 +0200

    feat: add @tanstack/react-query

--- packages/pinorama-studio/src/app.tsx
+++ packages/pinorama-studio/src/app.tsx
@@ -1,36 +1,5 @@
-import { ThemeProvider } from "@/components/theme-provider"
-import {
-  Menubar,
-  MenubarContent,
-  MenubarItem,
-  MenubarMenu,
-  MenubarSeparator,
-  MenubarShortcut,
-  MenubarTrigger
-} from "@/components/ui/menubar"
-
 function App() {
-  return (
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
-      <div className="p-4">
-        <Menubar>
-          <MenubarMenu>
-            <MenubarTrigger>File</MenubarTrigger>
-            <MenubarContent>
-              <MenubarItem>
-                New Tab <MenubarShortcut>⌘T</MenubarShortcut>
-              </MenubarItem>
-              <MenubarItem>New Window</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Share</MenubarItem>
-              <MenubarSeparator />
-              <MenubarItem>Print</MenubarItem>
-            </MenubarContent>
-          </MenubarMenu>
-        </Menubar>
-      </div>
-    </ThemeProvider>
-  )
+  return <div className="p-4">Pinorama Studio</div>
 }
 
 export default App
--- packages/pinorama-studio/src/main.tsx
+++ packages/pinorama-studio/src/main.tsx
@@ -1,10 +1,19 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query"
 import React from "react"
 import ReactDOM from "react-dom/client"
+
+import { ThemeProvider } from "@/components/theme-provider"
 import App from "./app.tsx"
 import "./globals.css"
 
+const queryClient = new QueryClient()
+
 ReactDOM.createRoot(document.getElementById("app") as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </ThemeProvider>
   </React.StrictMode>
 )
