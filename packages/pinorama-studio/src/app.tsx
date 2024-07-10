9b4c0b9a9eed12cc195b659dd10813b2ec84304e
Francesco Pasqua <cesconix@me.com>
2024-07-11 00:38:26 +0200

    feat(studio): add reset filters button

--- packages/pinorama-studio/src/app.tsx
+++ packages/pinorama-studio/src/app.tsx
@@ -1,11 +1,30 @@
+import { useState } from "react"
 import { LogExplorer } from "./components/log-explorer"
+import type { SearchFilters } from "./components/log-explorer/components/log-filters/types"
 import { TitleBar } from "./components/title-bar/title-bar"
 
 function App() {
+  const [searchText, setSearchText] = useState<string>("")
+  const [filters, setFilters] = useState<SearchFilters>({})
+
+  const hasFilters = searchText.length > 0 || Object.keys(filters).length > 0
+  const handleResetFilters = () => {
+    setSearchText("")
+    setFilters({})
+  }
+
   return (
     <div className="h-screen w-full grid grid-rows-[48px_1fr]">
-      <TitleBar />
-      <LogExplorer />
+      {/* Header */}
+      <TitleBar hasFilters={hasFilters} onResetFilters={handleResetFilters} />
+
+      {/* Container */}
+      <LogExplorer
+        searchText={searchText}
+        filters={filters}
+        onSearchTextChange={setSearchText}
+        onFiltersChange={setFilters}
+      />
     </div>
   )
 }
