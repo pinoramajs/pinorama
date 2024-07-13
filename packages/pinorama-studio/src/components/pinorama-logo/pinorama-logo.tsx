0d2903a4bd6bbb42c9892340695e4e0b43d0935c
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-07-13 08:05:06 +0000

    feat: pinorama logo

--- packages/pinorama-studio/src/components/pinorama-logo/pinorama-logo.tsx
+++ packages/pinorama-studio/src/components/pinorama-logo/pinorama-logo.tsx
@@ -1,13 +1,17 @@
 import { Shell } from "lucide-react"
+import { FormattedMessage } from "react-intl"
 
 export function PinoramaLogo() {
   return (
     <div className="flex items-center space-x-1.5 font-medium">
       <Shell className="h-[18px] w-[18px]" />
       <div className="leading-tight">
-        Pinorama{" "}
+        <FormattedMessage id="app.name" />
         <small className="text-xs font-normal text-muted-foreground">
-          v{import.meta.env.PACKAGE_VERSION}
+          <FormattedMessage
+            id="app.version"
+            values={{ version: import.meta.env.PACKAGE_VERSION }}
+          />
         </small>
       </div>
     </div>
