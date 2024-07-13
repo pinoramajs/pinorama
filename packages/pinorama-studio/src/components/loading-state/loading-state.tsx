8c83476220a67295bff5a69bda2bd5d24dcba620
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-07-13 07:58:13 +0000

    feat: labels loading

--- packages/pinorama-studio/src/components/loading-state/loading-state.tsx
+++ packages/pinorama-studio/src/components/loading-state/loading-state.tsx
@@ -1,10 +1,11 @@
 import { LoaderIcon } from "lucide-react"
+import { FormattedMessage } from "react-intl"
 
 export function LoadingState() {
   return (
     <div className="flex items-center h-10 p-3 m-2 text-sm text-muted-foreground">
       <LoaderIcon className="w-4 h-4 mr-2 animate-spin" />
-      Loading...
+      <FormattedMessage id="labels.loading" />
     </div>
   )
 }
