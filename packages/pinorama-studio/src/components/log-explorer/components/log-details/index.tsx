d6d5bd1af88632970a9c6f61a78325b1b87ebdfe
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-07-15 21:55:35 +0200

    feat: log-details

--- packages/pinorama-studio/src/components/log-explorer/components/log-details/index.tsx
+++ packages/pinorama-studio/src/components/log-explorer/components/log-details/index.tsx
@@ -1,10 +1,12 @@
+import style from './log-details.module.css';
+
 type LogDetailsProps = {
   data: any
 }
 
 export function LogDetails(props: LogDetailsProps) {
   return (
-    <div className="flex flex-col h-full p-3 overflow-auto">
+    <div className={style.container}>
       <pre className="text-sm">{JSON.stringify(props.data, null, 2)}</pre>
     </div>
   )
