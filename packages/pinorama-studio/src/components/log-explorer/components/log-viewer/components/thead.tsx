eac804ff3bf0941b667cff487ac1541a0c687bb3
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-07-15 22:27:20 +0200

    feat: thead

--- packages/pinorama-studio/src/components/log-explorer/components/log-viewer/components/thead.tsx
+++ packages/pinorama-studio/src/components/log-explorer/components/log-viewer/components/thead.tsx
@@ -1,15 +1,18 @@
 import { type Table, flexRender } from "@tanstack/react-table"
 
+import clsx from "clsx"
+import style from "./thead.module.css"
+
 export function TableHead({ table }: { table: Table<unknown> }) {
   return (
-    <thead className="group sticky top-0 z-10 bg-background select-none flex">
+    <thead className={clsx(style.head, "group")}>
       {table.getHeaderGroups().map((headerGroup) => (
         <tr key={headerGroup.id}>
           {headerGroup.headers.map((header) => (
             <th
               key={header.id}
               colSpan={header.colSpan}
-              className="relative px-3 mb-1 text-left font-normal text-muted-foreground align-middle h-9"
+              className={style.header}
               style={{ width: header.getSize() }}
             >
               {header.isPlaceholder
@@ -22,11 +25,10 @@ export function TableHead({ table }: { table: Table<unknown> }) {
                 <div
                   onMouseDown={header.getResizeHandler()}
                   onTouchStart={header.getResizeHandler()}
-                  className={`absolute right-0 top-0 h-[32px] w-[3px] bg-muted opacity-0 group-hover:opacity-100 cursor-col-resize select-none touch-none ${
-                    header.column.getIsResizing()
-                      ? "group-hover:bg-accent-foreground bg-accent-foreground"
-                      : ""
-                  }`}
+                  className={clsx(
+                    style.headerContent,
+                    header.column.getIsResizing() && style.resizing
+                  )}
                 />
               )}
             </th>
