5b4463c979457593a9c5c448338e30ee5f33d92d
Francesco Pasqua <cesconix@me.com>
2024-07-18 18:09:10 +0200

    fix(studio): add isolated root component to fix fast refresh

--- packages/pinorama-studio/src/main.tsx
+++ packages/pinorama-studio/src/main.tsx
@@ -1,34 +1,5 @@
-import "./styles/globals.css"
-
-import { StrictMode } from "react"
 import ReactDOM from "react-dom/client"
-
-import { TooltipProvider } from "@/components/ui/tooltip"
-import {
-  AppConfigProvider,
-  I18nProvider,
-  PinoramaClientProvider,
-  ThemeProvider
-} from "@/contexts"
-import App from "./app"
-
-const RootComponent = () => {
-  return (
-    <StrictMode>
-      <ThemeProvider defaultTheme="dark" storageKey="pinorama-studio-theme">
-        <I18nProvider>
-          <AppConfigProvider>
-            <PinoramaClientProvider>
-              <TooltipProvider>
-                <App />
-              </TooltipProvider>
-            </PinoramaClientProvider>
-          </AppConfigProvider>
-        </I18nProvider>
-      </ThemeProvider>
-    </StrictMode>
-  )
-}
+import { RootComponent } from "./root"
 
 const appElement = document.getElementById("app") as HTMLElement
 ReactDOM.createRoot(appElement).render(<RootComponent />)
