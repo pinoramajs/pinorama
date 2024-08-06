1696aabd76f30c731a4a846bcfed80a9b5659e55
Francesco Pasqua <cesconix@me.com>
2024-08-06 15:18:30 +0200

    chore(docs): add pinorama github link (#13)

--- packages/pinorama-docs/.vitepress/config.ts
+++ packages/pinorama-docs/.vitepress/config.ts
@@ -1,28 +1,26 @@
 import { defineConfig } from "vitepress"
 
-// https://vitepress.dev/reference/site-config
 export default defineConfig({
-  title: "Pinorama | Log Search for Pino",
+  title: "Pinorama",
   description: "Log Search for Pino",
   themeConfig: {
-    // https://vitepress.dev/reference/default-theme-config
     nav: [
-      { text: "Home", link: "/" },
-      { text: "Examples", link: "/markdown-examples" }
+      { text: "Home", link: "/" }
+      // { text: "Examples", link: "/markdown-examples" }
     ],
 
     sidebar: [
-      {
-        text: "Examples",
-        items: [
-          { text: "Markdown Examples", link: "/markdown-examples" },
-          { text: "Runtime API Examples", link: "/api-examples" }
-        ]
-      }
+      // {
+      //   text: "Examples",
+      //   items: [
+      //     { text: "Markdown Examples", link: "/markdown-examples" },
+      //     { text: "Runtime API Examples", link: "/api-examples" }
+      //   ]
+      // }
     ],
 
     socialLinks: [
-      { icon: "github", link: "https://github.com/vuejs/vitepress" }
+      { icon: "github", link: "https://github.com/pinoramajs/pinorama" }
     ]
   }
 })
