78ee99097ae793f2ba1e836ab5d839eb7b282a52
Matteo Pietro Dazzi <matteopietro.dazzi@gmail.com>
2024-08-10 11:56:42 +0200

    chore: title bar

--- packages/pinorama-studio/src/components/title-bar/title-bar.tsx
+++ packages/pinorama-studio/src/components/title-bar/title-bar.tsx
@@ -4,26 +4,35 @@ import { PinoramaLogo } from "./components/pinorama-logo"
 import { SettingsButton } from "./components/settings-button"
 import { ThemeToggleButton } from "./components/theme-toggle-button"
 
+const Left = () => (
+  <div className="flex items-center">
+    <PinoramaLogo />
+  </div>
+)
+
+const Center = () => (
+  <div className="absolute left-1/2 transform -translate-x-1/2 flex h-full items-center">
+    <ConnectionStatusButton />
+  </div>
+)
+
+const Right = () => (
+  <div className="flex items-center space-x-1.5">
+    <ThemeToggleButton />
+    <SettingsButton />
+    <ConnectionToggleButton />
+  </div>
+)
+
 export function TitleBar() {
   return (
     <div className="border-b bg-muted/20">
       <div className="relative flex justify-between text-sm h-full px-3">
-        {/* Left */}
-        <div className="flex items-center">
-          <PinoramaLogo />
-        </div>
+        <Left />
 
-        {/* Center */}
-        <div className="absolute left-1/2 transform -translate-x-1/2 flex h-full items-center">
-          <ConnectionStatusButton />
-        </div>
+        <Center />
 
-        {/* Right */}
-        <div className="flex items-center space-x-1.5">
-          <ThemeToggleButton />
-          <SettingsButton />
-          <ConnectionToggleButton />
-        </div>
+        <Right />
       </div>
     </div>
   )
